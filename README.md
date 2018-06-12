aa29e1da6890e98cdd6c0a7a5262531a03f9c278
Sang Mercado <smercado@linkedin.com>
2018-06-12 02:49:47 -0700

    Adding few other commands to extract links

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Install dependencies with
 
 > `yarn`
 
-Run the script with 
+Get all the pages link into `all-pages.txt`
 
-> `yarn scrape https://sfbay.craigslist.org/search/fbh?s=120 page2.txt`
+> `yarn scrape get-pages https://sfbay.craigslist.org/d/food-beverage-hospitality/search/fbh all-pages.txt`
+
+Get specific page description into `output.txt`
+
+> `yarn scrape scrape-page "https://sfbay.craigslist.org/search/fbh?s=2280" output.txt`
+
+Get bash script
+
+> `yarn scrape generate-script all-pages.txt output.txt commands.sh`
--- scrape.ts
+++ scrape.ts
@@ -1,10 +1,12 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
 
-const craigslistPage = process.argv[2];
-const filename = process.argv[3];
+type SupportedModes = "get-pages" | "scrape-page" | "generate-script"
+// Since we only support "scrapePage" or "getPages", something like
+// `commander.js` is probably over-kill.
+const mode: SupportedModes = process.argv[2] as SupportedModes;
 
-const logic = async () => {
+const scrapePage = async (craigslistPage: string, filename: string) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   await page.goto(craigslistPage);
@@ -49,9 +51,62 @@ const logic = async () => {
   });
 
   Promise.all(descriptions).then(result => {
-    fs.writeFileSync(filename, result.join('\n'));
+    fs.writeFileSync(filename, result.join('\n\n================================================\n\n'));
     browser.close();
   });
 };
 
-logic();
+const getPages = async (craigslistPage: string, filename: string) => {
+  const browser = await puppeteer.launch({ slowMo: 100, args: ['--no-sandbox'] });
+  const nextButtonSelector = '.button.next';
+  const page = await browser.newPage();
+  let urls: string[] = [];
+  await page.goto(craigslistPage);
+
+  try {
+    while (true) {
+      urls.push(page.url());
+      const nextButtonExists: boolean = await page.$(nextButtonSelector) != null;
+      if (nextButtonExists) {
+        await page.click(nextButtonSelector);
+      } else {
+        break;
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  fs.appendFileSync(filename, '\n');
+  fs.appendFileSync(filename, urls.join('\n'));
+  browser.close()
+}
+
+const scrapePages = async (inputFileName: string, outputFileName: string, resultFileName: string) => {
+  const urlStrings = fs.readFileSync(inputFileName, 'utf-8');
+  const urls: string[] = urlStrings
+    .split('\n')
+    .filter(Boolean)
+    .map(url => `yarn scrape scrape-page ${url} ${outputFileName}`);
+  fs.writeFileSync(resultFileName, urls.join('\n'));
+};
+
+if (process.argv.length === 5 || process.argv.length === 6) {
+  if (mode === 'scrape-page') {
+    const craigslistPage = process.argv[3];
+    const filename = process.argv[4];
+    scrapePage(craigslistPage, filename);
+  } else if(mode === 'get-pages') {
+    const craigslistPage = process.argv[3];
+    const filename = process.argv[4];
+    getPages(craigslistPage, filename);
+  } else if (mode === 'generate-script') {
+    const urlFile = process.argv[3];
+    const outputFileName = process.argv[4];
+    scrapePages(urlFile, outputFileName, process.argv[5]);
+  } else {
+    console.log('Mode not supported');
+  }
+} else {
+  console.log('Wrong number of args');
+}
