78724175e2aa2d8f138bf15298f47484ccb199d8
Sang Mercado <smercado@linkedin.com>
2019-01-22 10:50:52 -0800

    Adding support for single page apps

--- get-titles.ts
+++ get-titles.ts
@@ -27,8 +27,7 @@ const generateUniqueTitles = (titles: Array<ScrapedData>): Array<ScrapedData> =>
   return result;
 }
 
-const scrapeTitles = async (url: string, titleSelector: string, nextButtonSelector: string) => {
-
+const scrapeTitles = async (url: string, titleSelector: string, nextButtonSelector: string, nextPageRequest: string, sleepTime?: number) => {
   const browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox']
@@ -38,27 +37,52 @@ const scrapeTitles = async (url: string, titleSelector: string, nextButtonSelect
 
   await page.goto(url);
   await page.waitForSelector(nextButtonSelector);
+  await page.setRequestInterception(true);
 
-  let nextButtonExists: boolean = await page.$(nextButtonSelector) != null;
   let titles: Array<ScrapedData> = await extractTitles(page, titleSelector);
   let previousUrl: string = '';
+  let previousAjaxRequestUrl: string = '';
+  let ajaxRequestUrl: string = '';
 
-  if (!nextButtonExists) {
-    console.log('Next button was not found!');
-  }
+  page.on('request', request => {
+    if (request.url().includes(nextPageRequest)) {
+      ajaxRequestUrl = request.url();
+    }
+
+    request.continue();
+  });
 
   while (true) {
     try {
-      if (previousUrl === page.url()) {
+      const pageUrl = page.url();
+      if (!nextPageRequest && previousUrl === pageUrl) {
         console.log('Exiting due to the same page again', previousUrl);
         break;
       }
-      previousUrl = page.url();
-      console.log(`Scraping ${page.url()}`);
+
+      previousUrl = pageUrl;
+
+      if (nextPageRequest) {
+        console.log(`Scraping ${ajaxRequestUrl}`);
+      } else {
+        console.log(`Scraping ${pageUrl}`);
+      }
 
       await page.waitForSelector(nextButtonSelector, { timeout: 15000 });
+      await page.waitForSelector(titleSelector, { timeout: 15000 });
+
       titles = titles.concat(await extractTitles(page, titleSelector));
       await page.click(nextButtonSelector);
+
+      if (previousAjaxRequestUrl === ajaxRequestUrl) {
+        console.log('End of page', ajaxRequestUrl);
+        break;
+      }
+
+      previousAjaxRequestUrl = ajaxRequestUrl;
+      if (sleepTime) {
+        await sleep(sleepTime * 1000);
+      }
     } catch(err) {
       console.log('Error happened. Exiting gracefully: ', err);
       break;
@@ -77,5 +101,7 @@ const scrapeTitles = async (url: string, titleSelector: string, nextButtonSelect
 const url = process.argv[2];
 const titleSelector = process.argv[3];
 const nextButtonSelector = process.argv[4];
+const nextPageRequest = process.argv[5];
+const sleepTime: string = process.argv[6];
 
-scrapeTitles(url, titleSelector, nextButtonSelector);
+scrapeTitles(url, titleSelector, nextButtonSelector, nextPageRequest || '', parseInt(sleepTime) || undefined);
